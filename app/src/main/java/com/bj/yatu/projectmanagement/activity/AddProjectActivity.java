7be4b1e7597c8be790a534a1cc1e15b3f64de738
zw <zhangwei_7154@163.com>
2017-04-05 17:23:02 +0800

    添加标准字体颜色  color.xml中 添加标准字号   dimens.xml中

---
+++ app/src/main/java/com/bj/yatu/projectmanagement/activity/AddProjectActivity.java
@@ -0,0 +1,15 @@
+package com.bj.yatu.projectmanagement.activity;
+
+import android.os.Bundle;
+
+import com.bj.yatu.projectmanagement.R;
+
+public class AddProjectActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_project);
+
+    }
+}
